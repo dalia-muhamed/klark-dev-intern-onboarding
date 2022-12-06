d0a74b473b5c2ee08fffeaa8d0e11176a5cd9aa4
dalia.muhammed <dalia.muhammed1525@gmail.com>
2022-12-06 23:47:06 +0200

    add last card

--- base-code/index.php
+++ base-code/index.php
@@ -2,10 +2,9 @@
 <html lang="en">
 	<!--begin::Head-->
 	<head>
-		<base href="" />
 		<title>Home page</title>
 		<meta charset="utf-8" />
-		<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+		<!-- <link rel="shortcut icon" href="assets/media/logos/favicon.ico" /> -->
 		<!--begin::Fonts(mandatory for all pages)-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
 		<!--end::Fonts-->
@@ -26,21 +25,146 @@
 	<body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-sidebar-stacked="true" data-kt-app-toolbar-enabled="true" class="app-default">
 
 		<div id="app_container">
-	    <nav class="navbar">
-			<h5>Zuntia</h5>
-			<ul>
-				<li>Dashboard</li>
-				<li>Content Library</li>
+		<div id="app_container">
+    <nav class="navbar">
+       
+        <ul>
+            <h1>Zantia</h1>
+            <hr>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Content Library </a>
+            <ul><li><a href="#">- Overview</a></li>
+                <li><a href="#">- Posts</a></li>
+                <li><a href="#">- Campaigns</a></li>
+                <li><a href="#">- Content Calenders</a></li>
+                <li><a href="#">- Ideas</a></li>
+            </ul></li>
+         <li><a href="#">Drafts</a></li>
+         <li><a href="#">File Manager</a></li>
+         <li><a href="#">Team Manager</a></li>
+         <li><a href="#">Reporting</a></li>
+        </ul>
+    </nav>
 
-			</ul>
-		</nav>
+    <header class="header">
+        <form class="d-flex" role="seasrch" >
+            <div class="input-icons">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            <input  type="search" > </div> </form>
+            <div>
+            <button type="button" id="MybtnModal" class="btn btn-primary">open modal</button>
+            <button id="btn" class="btn btn-info" type="submit">@</button>
+            <button id="btn" class="btn btn-info" type="submit">@</button>
+        </div>
+            
+            <div class="morning">
+              <div>  Good morning
+                <h6>Brian Kamala</h6></div>
+                <div><img src="media/romano.jpg"></div>
+            </div>
+            
+    </header>
+<main class="main">
+    <article>
+        <h5> Where does it come from?</h5>
+       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+         Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+         unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting,
+         remaining essentially unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+           software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+         <h5> Where does it come from?</h5>
+        <p> It is a long established fact that a reader will be distracted by the readable
+         content of a page when looking at its layout. The point of using Lorem Ipsum is 
+         that it has a more-or-less normal distribution of letters, as opposed to using 
+         'Content here, content here', making it look like readable English. Many desktop
+          publishing packages and web page editors now use Lorem Ipsum as their default model
+           text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy
+           . Various versions have evolved over the years, sometimes by accident, sometimes on
+            purpose (injected humour and the like).</p>
+       </article>
+<aside class="aside">
+    <h1>aside here</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+      Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+      unknown printer took a galley of type and scrambled it to make a type specimen book.
+     It has survived not only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged. It was popularised in the 1960s with the release of
+       Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+        software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </aside>
+</main>
+<div class="container col-md-8">
+	<!-- .modal -->
+	<div class="modal fade" id="Mymodal">
+		<div class="modal-dialog">
+			<div class="modal-content" >
+				<div class="modal-header">
+					<div class="row d-flex">
+                        <div class="col d-flex">
+                            <h5>PREVIEW</h5>
+                            <button type="button" class="btn btn-success col-md-5 mx-auto">Approved</button></div>
+                        </div>
+                        <div class="col">
+                            <h5>ACTIVITY</h5>
+                        </div>
 
-		<div class="text-center mt-4 pt-5">
-			This page Works
-		</div>
+                    </div>
+					                                                         
+				</div> 
+				<div class="modal-body">
+					<div class="row d-flex">
+                        <div class="col d-flex">
+						<div class="card">
+                    <img src="media/donuts-pink-background-donuts-pink-background-tasty-dessert-heart-made-sweets-169972029.jpg"
+					class="card-img-top" style="height: 270px ;"> 
+                  
+                <div class="card-body">
+                <h5 >Milkey Dounts </h5>
+               <p class="card-text"> Our Delicious caster Dounts with some sprinkles on it.With regards to funding, the main sources of funding 
+				  business.</p></div>
+				  <hr>
+				<div class="card-footer">
+				<div class="accordion" id="acc1">
+      <div class="accordion-item">
+        <h1 class="accordion-header" id="head1">
+          <button  class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#open1">
+            Select Client</button>
+          </h1>
+        <div id="open1" class="accordion-collapse collapse" data-bs-parent="#acc1">
+          <div class="accordion-body">Hello world 1</div>
+        </div>
+      </div>
+				<div class="alert alert-warning text-dark alert-dismissible fade show">Select Client<button type="button" class="btn-close" data-bs-dismiss="alert"></button></div>
+				
+				</div>
+				</div>
+                        </div>
+                        <div class="col d-flex">
+							<img src="media/temmy.jpg" style="width:50px;height:50px;border-radius:15px">
+                            <div style="width:200px;height:100px; background-color:white">
+							<form class="d-flex" role="seasrch" style="margin-left: auto;" >
+                          <input class="form-control margin-left:2" type="search" placeholder="Replying ...." aria-label="Search"></form>
 
-		<!-- End::your code -->
-		</div>
+							<hr>
+							
+							<button  class="btn btn-warning"  type="submit">ADD Comment</button>
+						</div>
+                        </div>
+				</div>   
+				<div class="modal-footer">
+                    <div class="btngp1">
+					<button type="button" class="btn btn-danger" data-dismiss="modal">Delete</button>
+                    <button type="button" class="btn btn-light" >Show</button></div>
+                <div class="btngp2">
+                    <button type="button" class="btn btn-warning" >UNDO APPROVE</button>
+                    <button type="button" class="btn btn-primary" >Edit</button></div>
+				</div>
+			</div>                                                                       
+		</div>                                          
+	</div>
+</div>
 
 		<!--begin::Javascript-->
 		<script>var hostUrl = "scripts/lib/uikit/";</script>
